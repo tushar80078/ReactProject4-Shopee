e853ac417600200a98e905fa2e241b6e43ada068
tushar80078 <cplusplusonline22@gmail.com>
2023-04-18 13:14:27 +0530

    addedtocart

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ShowCart from './Components/ShowCart'
+import ShowData from './Components/ShowData'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    
+      <ShowData/>
     </div>
   )
 }
